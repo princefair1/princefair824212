7be05ef3c3e9dc1d26d0dba199b92c827ea12c8e
princefair1 <42935207+princefair1@users.noreply.github.com>
2018-09-03 14:01:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = 'ba.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Bazoka Site Soon`,"http://extbazoka.dx.am/skin/index.html")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
