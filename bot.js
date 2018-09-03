5985751525dcbedea49e654067d30198d1df7b0e
princefair1 <42935207+princefair1@users.noreply.github.com>
2018-09-03 14:11:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,17 +27,8 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame('ba. | *Soon');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
-});
-var prefix = "ba.";
 
- 
+var prefix = "ba.";
 client.on('message', message => { // كود برودكسات
     var prefix = 'ba.'; // هنا تقدر تغير البرفكس
 	var command = message.content.split(" ")[0];
